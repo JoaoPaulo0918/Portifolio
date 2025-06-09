c61cfd22b61886187fe9a925643c44d357941c8c
Joao Paulo <js0918990@gmail.com>
2025-06-09 18:50:46 -0300

    Updates

---
+++ images/Img.jsx
@@ -0,0 +1,49 @@
+/*icons programação*/ 
+import reactLogo from '../../src/assets/react.png'
+import viteLogo from '../../public/vite.svg'
+import logoHtml from '../../src/assets/html.png'
+import logoCss from '../../src/assets/css.png'
+import logoJavaScript from '../../src/assets/javascript.png'
+import logoBoot from '../../src/assets/icons8-bootstrap-48.png'
+import logoNodeJs from '../../src/assets/node-js.png'
+import logoMongo from '../../src/assets/mongodb.png'
+import logoPhp from '../../src/assets/php-brands (1).svg'
+import logoMysql from '../../src/assets/icons8-mysql-48.png'
+import logoSass from '../../src/assets/icons8-atrevimento-50.png'
+import logogit from '../../src/assets/icons8-git-48.png'
+import logogithub from '../../src/assets/icons8-github-50.png'
+import logoVs from '../../src/assets/icons8-visual-studio-48.png'
+
+/*logo pagina*/
+import logoPng from '../../src/assets/logoweb.png'
+
+
+/*imagens pessoais*/
+import minhaFoto from '../../src/assets/eu.jpg'
+
+/*imagens de projetos*/
+import proEstoque from '../../src/assets/ProjetoEstoque.jpg'
+import proVaga from '../../src/assets/ProjetoVagas.jpg'
+import proPortifolio from '../../src/assets/ProPortifolio.jpg'
+
+export default {
+    reactLogo,
+    viteLogo,
+    logoPng,
+    logoCss,
+    logoHtml,
+    logoJavaScript,
+    logoNodeJs,
+    logoPhp,
+    minhaFoto,
+    proEstoque,
+    proVaga,
+    proPortifolio,
+    logoSass,
+    logoBoot,
+    logoMongo,
+    logoMysql,
+    logogit,
+    logogithub,
+    logoVs
+}
